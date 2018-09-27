d7cfa963fb9fbb45c96e3413ae4bb0c334aee3ea
Tal <tal.bny3@gmail.com>
2018-09-27 20:53:16 +0300

    added tool bar to the new layout, and added a function for the button in the start layout which goes to the new layout

--- app/src/main/java/com/taban/learnenglish/MainActivity.java
+++ app/src/main/java/com/taban/learnenglish/MainActivity.java
@@ -2,6 +2,7 @@ package com.taban.learnenglish;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void changeToLearningLayout(View view) {
+        setContentView(R.layout.words_to_memorize);
     }
 }
--- app/src/main/java/com/taban/learnenglish/WordsToMemorizeActivity.java
+++ app/src/main/java/com/taban/learnenglish/WordsToMemorizeActivity.java
@@ -1,4 +1,17 @@
 package com.taban.learnenglish;
 
-public class WordsToMemorizeActivity {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+
+public class WordsToMemorizeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.words_to_memorize);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+    }
 }
