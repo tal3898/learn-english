a06fbe10bee8f24c03e0ca5d18b633d22d8f1717
Tal <tal.bny3@gmail.com>
2018-09-28 16:06:42 +0300

    created words manager

---
+++ app/src/main/java/com/taban/learnenglish/utilities/WordsManager.java
@@ -0,0 +1,24 @@
+package com.taban.learnenglish.utilities;
+
+import com.taban.learnenglish.models.Difficulty;
+import com.taban.learnenglish.models.Word;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WordsManager {
+    private List<Word> newWordsToMemorize;
+
+    public WordsManager() {
+        this.newWordsToMemorize = new ArrayList<>();
+    }
+
+    // Methods
+    public void loadAllWords() {
+        this.newWordsToMemorize.add(new Word("Hello", "shalom", new ArrayList<String>(), null, Difficulty.BEGINNER));
+        this.newWordsToMemorize.add(new Word("Hello", "shalom", new ArrayList<String>(), null, Difficulty.BEGINNER));
+        this.newWordsToMemorize.add(new Word("Hello", "shalom", new ArrayList<String>(), null, Difficulty.BEGINNER));
+        this.newWordsToMemorize.add(new Word("Hello", "shalom", new ArrayList<String>(), null, Difficulty.BEGINNER));
+        this.newWordsToMemorize.add(new Word("Hello", "shalom", new ArrayList<String>(), null, Difficulty.BEGINNER));
+    }
+}
