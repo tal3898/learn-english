9f16e45eb8ede4f0bf6a07945d32f2b66b09172f
Tal <tal.bny3@gmail.com>
2018-10-05 17:09:56 +0300

    removed com.baoyz.swipemenulistview from the gradle and its references

--- app/src/main/java/com/taban/learnenglish/activities/WordsToMemorizeActivity.java
+++ app/src/main/java/com/taban/learnenglish/activities/WordsToMemorizeActivity.java
@@ -1,35 +1,20 @@
 package com.taban.learnenglish.activities;
 
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
-import android.media.MediaPlayer;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.annotation.RequiresApi;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ListAdapter;
 import android.widget.ListView;
-
-import com.baoyz.swipemenulistview.SwipeMenu;
-import com.baoyz.swipemenulistview.SwipeMenuCreator;
-import com.baoyz.swipemenulistview.SwipeMenuItem;
-import com.baoyz.swipemenulistview.SwipeMenuListView;
 import com.taban.learnenglish.R;
 import com.taban.learnenglish.SettingsActivity;
 import com.taban.learnenglish.adpters.WordsListAdapter;
-import com.taban.learnenglish.models.Word;
 import com.taban.learnenglish.utilities.WordsManager;
-
-import java.io.File;
-import java.io.IOException;
 import java.io.Serializable;
 
 public class WordsToMemorizeActivity extends AppCompatActivity {
@@ -58,38 +43,6 @@ public class WordsToMemorizeActivity extends AppCompatActivity {
 
         wordsListView = (ListView)findViewById(R.id.wordsListView);
         wordsListView.setAdapter(wordsListAdapter);
-
-
-
-        // Create the swipe menu in the list view
-        SwipeMenuCreator creator = new SwipeMenuCreator() {
-
-            @Override
-            public void create(SwipeMenu menu) {
-                // create "Memorized" item
-                SwipeMenuItem memorizedItem = new SwipeMenuItem(
-                        getApplicationContext());
-
-                memorizedItem.setBackground(R.color.memorizedWordItem);
-                memorizedItem.setWidth(270);
-                memorizedItem.setTitle("O");
-                memorizedItem.setTitleSize(18);
-                memorizedItem.setTitleColor(Color.BLACK);
-
-                // create "delete" item
-                SwipeMenuItem deleteItem = new SwipeMenuItem(
-                        getApplicationContext());
-                deleteItem.setBackground(R.color.deleteWordItem);
-                deleteItem.setWidth(270);
-                deleteItem.setIcon(R.drawable.ic_settings_wheel);
-
-                // add items to menu
-                menu.addMenuItem(memorizedItem);
-                menu.addMenuItem(deleteItem);
-            }
-        };
-
-
 
         // Initi the floating button
         wordPlayBtn = (FloatingActionButton) findViewById(R.id.words_play_btn);
