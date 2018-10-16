29bcddebe5c52220ee5d3967363affb45ba5149f
Tal <tal.bny3@gmail.com>
2018-10-16 17:45:40 +0300

    revert "bad code test2"
    This reverts commit a29ab290ce6a73b68e8eef94de24b7ed1dff580e.

--- app/src/main/java/com/taban/learnenglish/activities/SpecificWordActivity.java
+++ app/src/main/java/com/taban/learnenglish/activities/SpecificWordActivity.java
@@ -46,7 +46,6 @@ public class SpecificWordActivity extends AppCompatActivity {
 
     public void playWord(View view) {
         Globals.wordsAudioManager.getWordMediaPlayer(word).start();
-        // another bad code
     }
 
 }
