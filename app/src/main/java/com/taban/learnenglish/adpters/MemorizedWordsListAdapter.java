8617ab8ab13b26fcacfcd6de34bd876a5fdb9c99
Tal <tal.bny3@gmail.com>
2018-10-16 23:11:17 +0300

    now when pressing the info button in the memorized words list, going to the word info

--- app/src/main/java/com/taban/learnenglish/adpters/MemorizedWordsListAdapter.java
+++ app/src/main/java/com/taban/learnenglish/adpters/MemorizedWordsListAdapter.java
@@ -1,6 +1,7 @@
 package com.taban.learnenglish.adpters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,8 +10,10 @@ import android.widget.TextView;
 
 import com.taban.learnenglish.R;
 import com.taban.learnenglish.activities.LearnEnglishApplication;
+import com.taban.learnenglish.activities.SpecificWordActivity;
 import com.taban.learnenglish.models.Word;
 
+import java.io.Serializable;
 import java.util.List;
 
 public class MemorizedWordsListAdapter extends ArrayAdapter<Word> {
@@ -25,10 +28,23 @@ public class MemorizedWordsListAdapter extends ArrayAdapter<Word> {
         View wordItem = inflater.inflate(R.layout.memorized_word_item, parent, false);
         final Word currentWordItem = (Word) getItem(position);
 
+        // set the item text
         TextView wordTextView = wordItem.findViewById(R.id.word);
         TextView definitionTextView = wordItem.findViewById(R.id.definition);
         wordTextView.setText(currentWordItem.getWord());
         definitionTextView.setText(currentWordItem.getDefinition());
+
+        // define the info button
+        wordItem.findViewById(R.id.wordInfo).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent wordsPlayerActivityIntent = new Intent(LearnEnglishApplication.getAppContext(), SpecificWordActivity.class);
+                wordsPlayerActivityIntent.putExtra("wordData", (Serializable) currentWordItem);
+                wordsPlayerActivityIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                LearnEnglishApplication.getAppContext().startActivity(wordsPlayerActivityIntent);
+            }
+
+        });
 
         return wordItem;
     }
