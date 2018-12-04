08058b9b7a6fa0a0749710c33954da8da1f29ed9
Tal <tal.bny3@gmail.com>
2018-12-04 22:45:02 +0200

    added an empty mp3 file 'word_does_not_exist_audio', and now playing it when a word does not exist

--- app/src/main/java/com/taban/learnenglish/utilities/WordsAudioManager.java
+++ app/src/main/java/com/taban/learnenglish/utilities/WordsAudioManager.java
@@ -10,12 +10,15 @@ import java.util.Map;
 
 public class WordsAudioManager {
 
+    private final String WORD_DOES_NOT_EXIST_AUDIO_FILE_NAME = "word_does_not_exist_audio";
+
     // DM
     private Map<String, MediaPlayer> wordsAudioFiles;
 
-    // CTRO
+    // CTOR
     public WordsAudioManager() {
         wordsAudioFiles = new HashMap<>();
+        putTheEmptyAudioWord();
     }
 
     // Methods
@@ -32,10 +35,17 @@ public class WordsAudioManager {
                 MediaPlayer mediaPlayer = MediaPlayer.create(LearnEnglishApplication.getAppContext(), resId);
                 wordsAudioFiles.put(word, mediaPlayer);
             } else {
-                wordsAudioFiles.put(word, null);
+                wordsAudioFiles.put(word, wordsAudioFiles.get(WORD_DOES_NOT_EXIST_AUDIO_FILE_NAME));
             }
         }
 
         return wordsAudioFiles.get(word);
+    }
+
+    private void putTheEmptyAudioWord() {
+        int resId = LearnEnglishApplication.getAppContext().getResources().getIdentifier(
+                WORD_DOES_NOT_EXIST_AUDIO_FILE_NAME, "raw", LearnEnglishApplication.getAppContext().getPackageName());
+        MediaPlayer mediaPlayer = MediaPlayer.create(LearnEnglishApplication.getAppContext(), resId);
+        wordsAudioFiles.put(WORD_DOES_NOT_EXIST_AUDIO_FILE_NAME, mediaPlayer);
     }
 }
